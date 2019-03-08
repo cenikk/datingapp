660bd5c65f8c0dbd177ad165c218e93df5dd6d3e
cenikk <k.cenik@live.nl>
2019-03-08 14:04:53 +0100

    install ejs and express, add a 'view' folder with multiple partials and a 'static' folder with the first version of the dating site

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-var camelcase = require('camelcase');
+const express = require('express');
+const app = express(); // https://www.npmjs.com/package/express
+const ejs = require('ejs')
 
-console.log(camelcase('hello there'));
+const port = 8000;
+
+const testData = [
+    {
+        name: "Kaan",
+        age: 21,
+    },
+    {
+        name: "Kris",
+        age: 22
+    },
+]
+
+app
+    .set('view engine', 'ejs')
+    .set('views', 'view')
+
+    .get('/', helloWorld)
+    .get('/about', about)
+
+    .use(pageNotFound);
+
+// app.use('/static', express.static('public'));
+
+// http://localhost:8000/static/css/style.css
+
+app.listen(port)
+
+function helloWorld(req, res) {
+    res.render('not-found.ejs', {testData: testData});
+}
+
+function about(req, res) {
+    res.send('This is the about page');
+}
+
+function pageNotFound(req, res) {
+    res.status(404).render('not-found.ejs');
+}
