0f8ed1033151db82d4ae553583de90625c3187d9
cenikk <k.cenik@live.nl>
2019-03-08 19:59:44 +0100

    added comments to my index.js to make it more user-friendly for myself and 3rd parties, added a register page aswell

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
+// Require (load) NPM Modules 
 const express = require('express');
-const app = express(); // https://www.npmjs.com/package/express
-const ejs = require('ejs')
+const app = express(); 
+const ejs = require('ejs');
 
-const port = 8000;
+const port = 8000; 
 
-const testData = [
+// Added some test data to see how I can include it in my .ejs files.
+let testData = [
     {
         name: "Kaan",
         age: 21,
     },
     {
-        name: "Kris",
+        name: "Jaap",
         age: 22
     },
 ]
 
+// Placed some basic methods to my app (express), with help from: https://www.npmjs.com/package/express
 app
-    .set('view engine', 'ejs')
-    .set('views', 'view')
+    // Configure settings for express
+    .set('view engine', 'ejs') // use ejs for templating
+    .set('views', 'view') // load templates from 'view'
 
-    .get('/', helloWorld)
+    // Makes different routes (Method(Path, Handler))
+    .get('/', index)
     .get('/about', about)
+    .get('/register', register)
 
-    .use(pageNotFound);
+    // Use the following code to serve images, CSS files and JS in a directory called "static"
+    .use('/static', express.static('static'))
 
-// app.use('/static', express.static('public'));
+    // Use function pageNotFound when a route can't be found
+    .use(pageNotFound)
 
-// http://localhost:8000/static/css/style.css
+    // Listen for requests on port (8000)
+    .listen(port)
 
-app.listen(port)
+    
 
-function helloWorld(req, res) {
-    res.render('not-found.ejs', {testData: testData});
+// Handle the index request by rendering index.ejs. req = request, res = response
+function index(req, res) {
+    res.render('index.ejs', {testData: testData}); // render by combining templates with data, send the result
 }
 
 function about(req, res) {
     res.send('This is the about page');
+}
+
+function register(req, res) {
+    res.render('register.ejs');
 }
 
 function pageNotFound(req, res) {
