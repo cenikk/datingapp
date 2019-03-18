0a2e594f25110ed49bda534f7731cde9b39d4685
kriskuiper <kriskuiper74@gmail.com>
2019-03-18 19:27:17 +0100

    Make index.js more consistent 1. Make function names say what they do 2. Add listening function to display listening port in the console

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const express = require('express');
 const slugify = require('slugify');
 const bodyParser = require('body-parser');
 
-const port = 8000; 
+const port = 8000;
+const app = express();
 
 // Added some test data to see how I can include it in my .ejs files.
 let testData = [];
 
 // Placed some basic methods to my app (express), with help from: https://www.npmjs.com/package/express
-express()
+app
     // Use the following code to serve images, CSS files and JS in a directory called "static"
     .use('/static', express.static('static'))
     .use(bodyParser.urlencoded({ extended: false }))
@@ -19,56 +20,56 @@ express()
     .set('views', 'view')
 
     // Makes different routes (Method(Path, Handler))
-    .get('/', index)
-    .get('/about', about)
-    .get('/register', register)
-    .get('/:id', users)
-    .delete('/:id', remove)
+    .get('/', renderIndex)
+    .get('/about', renderAbout)
+    .get('/register', renderRegister)
+    .get('/:id', renderUserList)
+    .delete('/:id', removeUser)
 
-    .post('/', add)
+    .post('/', addUser)
 
     // Use function pageNotFound when a route can't b`e found
-    .use(pageNotFound)
+    .use(renderPageNotFound)
 
     //Listen for requests on port (8000)
     .listen(port, listening);
 
 // Handle the index request by rendering index.ejs. req = request, res = response
-function index(req, res) {
+function renderIndex(req, res) {
     res.render('index.ejs', {testData: testData}); // render by combining templates with data, send the result
 }
 
-function about(req, res) {
+function renderAbout(req, res) {
     res.send('This is the about page');
 }
 
-function register(req, res) {
+function renderRegister(req, res) {
     res.render('register.ejs');
 }
 
-function pageNotFound(req, res) {
+function renderPageNotFound(req, res) {
     res.status(404).render('not-found.ejs');
 }
 
-function add(req, res) {
+function addUser(req, res) {
     let id = slugify(req.body.voornaam).toLowerCase();
     testData.push({
         id: id,
-        firstName: req.body.voornaam,
+        firstName: req.body.firstname,
         gender: req.body.gender,
-        birthday: req.body.dag,
-        birthmonth: req.body.maand,
-        birthyear: req.body.jaar
+        birthday: req.body.day,
+        birthmonth: req.body.month,
+        birthyear: req.body.year
     });
 
     res.redirect('/' + id);
 }
 
-function users(req, res) {
+function renderUserList(req, res) {
     res.render('userlist.ejs', {testData});
 }
 
-function remove(req, res) {
+function removeUser(req, res) {
     let id = req.params.id;
 
     testData = testData.filter(function (value) {
