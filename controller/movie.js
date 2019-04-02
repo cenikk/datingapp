b4e3f046ad809a035dd11a4b242f6787c192d7a6
cenikk <k.cenik@live.nl>
2019-04-02 12:07:43 +0200

    movie and addmovie controller, new routes

---
+++ controller/movie.js
@@ -0,0 +1,5 @@
+function movie(req, res) {
+    res.render('movie.ejs');
+}
+
+module.exports = movie;
--- index.js
+++ index.js
@@ -53,6 +53,7 @@ const remove = require('./controller/remove.js');
 const loginValidation = require('./controller/loginValidation.js');
 const addUser = require('./controller/addUser.js');
 const pageNotFound = require('./controller/pageNotFound.js');
+const movie = require('./controller/movie.js');
 
 // Adding methods to my app (express)
 express()
@@ -74,11 +75,13 @@ express()
     .get('/:id', redirectLogin, profile) // Homepage after login
     .get('/:id/matches', redirectLogin, matches)
     .get('/:id/logout', redirectLogin, logout)
+    .get('/:id/movie', redirectLogin, movie)
     
     .delete('/:id', remove)
     
     .post('/login', loginValidation)
     .post('/register', uploadFolder.single('profilepicture'), addUser)
+    .post('/:id/movie', addMovie)
     
     .use(pageNotFound)
     
